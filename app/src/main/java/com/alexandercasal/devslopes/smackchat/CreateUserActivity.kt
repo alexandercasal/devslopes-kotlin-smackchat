b6845d40e0fd2af1960ca543fbc69b81fb1bc960
Alexander Casal <alexandericasal@gmail.com>
2017-10-13 16:16:03 -0600

    Added random avatar generation

--- app/src/main/java/com/alexandercasal/devslopes/smackchat/CreateUserActivity.kt
+++ app/src/main/java/com/alexandercasal/devslopes/smackchat/CreateUserActivity.kt
@@ -1,10 +1,16 @@
 package com.alexandercasal.devslopes.smackchat
 
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import kotlinx.android.synthetic.main.activity_create_user.*
+import java.util.*
 
 class CreateUserActivity : AppCompatActivity() {
+
+    var userAvatar = "profileDefault"
+    var avatarColor = "[0.5, 0.5, 0.5, 1]"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -12,11 +18,33 @@ class CreateUserActivity : AppCompatActivity() {
     }
 
     fun generateUserAvatar(view: View) {
+        var random = Random()
+        val color = random.nextInt(2)
+        val avatarNum = random.nextInt(28)
 
+        if (color == 0) {
+            userAvatar = "light$avatarNum"
+        } else {
+            userAvatar = "dark$avatarNum"
+        }
+
+        val resourceId = resources.getIdentifier(userAvatar, "drawable", this.packageName)
+        createAvatarImageView.setImageResource(resourceId)
     }
 
     fun generateColorClicked(view: View) {
+        val random = Random()
+        val r = random.nextInt(256)
+        val g = random.nextInt(256)
+        val b = random.nextInt(256)
 
+        createAvatarImageView.setBackgroundColor(Color.rgb(r, g, b))
+
+        val savedR = r.toDouble() / 255
+        val savedG = g.toDouble() / 255
+        val savedB = g.toDouble() / 255
+
+        avatarColor = "[$savedR, $savedG, $savedB, 1]"
     }
 
     fun createUserClicked(view: View) {
