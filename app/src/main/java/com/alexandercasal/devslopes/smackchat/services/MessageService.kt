2578d024813139bc557258e4f21014658cf83557
Alexander Casal <alexandericasal@gmail.com>
2017-10-16 21:41:49 -0600

    Receives messages relevant to selected channel and clears messages when switching channels

--- app/src/main/java/com/alexandercasal/devslopes/smackchat/services/MessageService.kt
+++ app/src/main/java/com/alexandercasal/devslopes/smackchat/services/MessageService.kt
@@ -6,6 +6,7 @@ import com.alexandercasal.devslopes.smackchat.controller.App
 import com.alexandercasal.devslopes.smackchat.model.Channel
 import com.alexandercasal.devslopes.smackchat.model.Message
 import com.alexandercasal.devslopes.smackchat.utils.URL_GET_CHANNELS
+import com.alexandercasal.devslopes.smackchat.utils.URL_GET_MESSAGES
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonArrayRequest
 import org.json.JSONException
@@ -49,5 +50,52 @@ object MessageService {
         }
 
         App.prefs.requestQueue.add(channelsRequest)
+    }
+
+    fun getMessages(chanelId: String, complete: (Boolean) -> Unit) {
+        val url = "$URL_GET_MESSAGES$chanelId"
+        val messageRequest = object : JsonArrayRequest(Method.GET, url, null, Response.Listener { response ->
+            clearMessages()
+            try {
+                for (x in 0 until response.length()) {
+                    val message = response.getJSONObject(x)
+                    val msgBody = message.getString("messageBody")
+                    val channelId = message.getString("channelId")
+                    val id = message.getString("_id")
+                    val userName = message.getString("userName")
+                    val userAvatar = message.getString("userAvatar")
+                    val userAvatarColor = message.getString("userAvatarColor")
+                    val timeStamp = message.getString("timeStamp")
+
+                    val newMessage = Message(msgBody, userName, channelId, userAvatar, userAvatarColor, id, timeStamp)
+                    messages.add(newMessage)
+                }
+                complete(true)
+            } catch (e: JSONException) {
+                Log.d("MessageService", "EXC: ${e.localizedMessage}")
+            }
+        }, Response.ErrorListener { error ->
+            Log.d("MessageService", "Unable to get messages")
+        }) {
+            override fun getHeaders(): MutableMap<String, String> {
+                val headers = HashMap<String, String>()
+                headers.put("Authorization", "Bearer ${App.prefs.authToken}")
+                return headers
+            }
+
+            override fun getBodyContentType(): String {
+                return "application/json; charset=utf-8"
+            }
+        }
+
+        App.prefs.requestQueue.add(messageRequest)
+    }
+
+    fun clearMessages() {
+        messages.clear()
+    }
+
+    fun clearChannels() {
+        channels.clear()
     }
 }
